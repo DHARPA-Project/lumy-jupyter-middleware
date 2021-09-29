bee0b339bde57c430120ab71fb3a69190911e814
Roman Kalyakin <roman@kalyakin.com>
2021-09-29 08:49:17 +0200

    extra unit tests and more mypy experiments

---
+++ test/workflow/get_list.py
@@ -0,0 +1,28 @@
+from lumy_middleware.jupyter.base import MessageEnvelope
+from lumy_middleware.target import Target
+from lumy_middleware.types.generated import (MsgWorkflowGetWorkflowList,
+                                             MsgWorkflowWorkflowList)
+from lumy_middleware.utils.unittest import ControllerTestCase
+from lumy_middleware.utils.dataclasses import from_dict
+
+
+class TestGetWorkflowList(ControllerTestCase):
+
+    def test_no_workflow_by_default(self):
+        def handler(msg: MessageEnvelope):
+            self.assertEqual(msg.action, 'WorkflowList')
+
+            content = from_dict(MsgWorkflowWorkflowList, msg.content)
+            self.assertGreater(len(content.workflows), 0)
+            first_workflow = content.workflows[0]
+            self.assertIsNotNone(first_workflow.body)
+
+        sub = self.client.subscribe(Target.Workflow, handler)
+        self.client.publish(
+            Target.Workflow,
+            MessageEnvelope(action='GetWorkflowList',
+                            content=MsgWorkflowGetWorkflowList(
+                                include_workflow=True
+                            ))
+        )
+        sub.unsubscribe()
